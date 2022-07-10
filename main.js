e63e6005efb94e24b62cc8617498d6bb7e8f6c5c
AOKA <amin.aoka@gmail.com>
2022-07-10 12:53:58 +0430

    commented

---
+++ main.js
@@ -0,0 +1,43 @@
+let number = document.querySelector("#number");
+let dec = document.querySelector(".dec");
+let res = document.querySelector(".res");
+let inc = document.querySelector(".inc");
+let aut = document.querySelector(".aut")
+let i=0;
+let c=1;
+let auto = true;
+let myInterval;
+
+let adder = function (){
+    i+=1;
+    number.innerText=i;
+
+}
+let decreaser = () => {
+    i--;
+    number.innerText = i;
+}
+
+let reseat = ()=> {
+    i=0;
+    number.innerText=i;
+    myInterval = setInterval(adder,2000)
+
+}
+
+let = automation = ()=>{
+
+clearInterval(myInterval)
+
+}
+
+// event listeners
+inc.addEventListener("click", adder);
+dec.addEventListener("click",decreaser)
+res.addEventListener("click", reseat)
+aut.addEventListener("click", automation)
+
+
+if (auto){
+    myInterval = setInterval(adder,2000)
+}
